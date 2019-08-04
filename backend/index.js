3975692afa0278b9d63e09d133d7e90b79f776e8
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2019-08-04 15:29:19 +0200

    Added API

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,73 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = process.env.PORT || 4000;
+const mongoose = require('mongoose');
+const todoRoutes = express.Router();
+const PORT = 4000;
+
+const Todo = require('./model');
 
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/todos-mern', {
+  useNewUrlParser: true
+});
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+  console.log('MongoDB database connection established successfully');
+});
+
+todoRoutes.route('/').get(function(req, res) {
+  Todo.find(function(err, todos) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+todoRoutes.route('/:id').get(function(req, res) {
+  let id = req.params.id;
+  Todo.findById(id, function(err, todo) {
+    res.json(todo);
+  });
+});
+
+todoRoutes.route('/update/:id').post(function(req, res) {
+  Todo.findById(req.params.id, function(err, todo) {
+    if (!todo) res.status(404).send('data is not found');
+    else todo.description = req.body.description;
+    todo.responsible = req.body.responsible;
+    todo.priority = req.body.priority;
+    todo.completed = req.body.completed;
+
+    todo
+      .save()
+      .then(todo => {
+        res.json('Todo updated!');
+      })
+      .catch(err => {
+        res.status(400).send('Update not possible');
+      });
+  });
+});
+
+todoRoutes.route('/add').post(function(req, res) {
+  let todo = new Todo(req.body);
+  todo
+    .save()
+    .then(todo => {
+      res.status(200).json({ todo: 'todo added successfully' });
+    })
+    .catch(err => {
+      res.status(400).send('adding new todo failed');
+    });
+});
+
+app.use('/todos', todoRoutes);
 
 app.listen(PORT, function() {
   console.log('Server is running on Port: ' + PORT);
