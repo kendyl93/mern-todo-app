c641a70c6c5831add028b4e8a6172b36091b6717
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2019-08-11 20:36:49 +0200

    Configure more webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,32 +1,14 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const VENDOR_LIBS = ['react', 'react-dom'];
-
 module.exports = {
   entry: {
-    entry: ['@babel/polyfill', './src/index.js'],
-    vendor: VENDOR_LIBS
+    entry: ['@babel/polyfill', './src/index.js']
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].[chunkhash].bundle.js',
-    chunkFilename: '[name].[chunkhash].bundle.js'
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          chunks: 'initial',
-          name: 'vendor',
-          test: 'vendor',
-          enforce: true
-        }
-      }
-    },
-    runtimeChunk: true
+    filename: 'bundle.js'
   },
   module: {
     rules: [
@@ -54,11 +36,7 @@ module.exports = {
       template: './src/index.html'
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      filename: 'main.css'
     })
   ]
 };
